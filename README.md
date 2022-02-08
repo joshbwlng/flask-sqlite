215f51836ced1a3927bbe08a9545460d19692610
Josh Bowling <josh@balena.io>
2022-02-08 11:35:56 +0900

    Add frontend

--- README.md
+++ README.md
@@ -3,17 +3,20 @@
 Example app for adding/viewing/deleting users stored in an SQLite database.  
 Python + Flask, runs on Balena.
 
+Backend service listens on port 3001, while frontend service listens on port 80.  
+The frontend service, when accessed with `http://<DEVICE-IP>/` should display `Hello from backend`, a message retrieved from the backend service.
+
 **Add user to database**
 ```
-curl -d '{"name":"foo","email":"bar@example.com"}' -H "Content-Type: application/json" -X POST http://<YOUR-IP>
+curl -d '{"name":"foo","email":"bar@example.com"}' -H "Content-Type: application/json" -X POST http://<YOUR-IP>:3001/api/users
 ```
 
 **Query for users by name**
 ```
-curl http://<YOUR-IP>\?name\=foo
+curl http://<YOUR-IP>:3001/api/users\?name\=foo
 ```
 
 **Delete stored user by name**
 ```
-curl -X DELETE http://<YOUR-IP>\?name\=foo
+curl -X DELETE http://<YOUR-IP>:3001/api/users\?name\=foo
 ```
--- backend/src/main.py
+++ backend/src/main.py
@@ -1,10 +1,14 @@
 import json
 import os
+from pathlib import Path
 import sqlite3
 from flask import Flask, request, g
 
 DATABASE = os.path.join(os.getcwd(), 'database.db')
-print(DATABASE)
+if not os.path.isfile(DATABASE):
+    print('Touching db:', DATABASE)
+    Path(DATABASE).touch()
+
 app = Flask(__name__)
 
 def get_db():
@@ -21,7 +25,11 @@ def close_connection(exception):
     if db is not None:
         db.close()
 
-@app.route('/', methods=['GET'])
+@app.route('/api', methods=['GET'])
+def hello_world():
+    return json.dumps({'message': 'Hello from backend'})
+
+@app.route('/api/users', methods=['GET'])
 def query_records():
     cur = get_db().cursor()
     name = request.args.get('name')
@@ -29,7 +37,7 @@ def query_records():
     results = cur.fetchall()
     return json.dumps(results)
 
-@app.route('/', methods=['POST'])
+@app.route('/api/users', methods=['POST'])
 def create_record():
     con = get_db()
     cur = con.cursor()
@@ -42,7 +50,7 @@ def create_record():
     results = cur.fetchall()
     return json.dumps(results)
 
-@app.route('/', methods=['DELETE'])
+@app.route('/api/users', methods=['DELETE'])
 def delete_record():
     con = get_db()
     cur = con.cursor()
@@ -52,4 +60,4 @@ def delete_record():
     return 'OK'
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=3000)
+    app.run(debug=True, host='0.0.0.0', port=3001)
